f0fabbcf699199a65c489ac5cd67a3e50b72dbf2
Abishek Bupathi <abishekb128@gmail.com>
2022-07-21 21:02:19 +0530

    Added function to upload help document

--- Application/Help/help.py
+++ Application/Help/help.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import webbrowser
 from xml.dom import minidom
@@ -10,15 +11,18 @@ from PySide2.QtGui import *
 from PySide2.QtWebEngineWidgets import *
 
 HELP_DOC_FILE_PATH = "./Help/help.md"
+HELP_DOC_FILE_DIR = "./Help/"
 
 class Help(QWidget):
 
     def __init__(self):
         super().__init__()
 
+        self.uploadButton = QPushButton("Upload Help doc")
+
         self.webEngineView = QWebEngineView()
 
-        self.mainLayout = QHBoxLayout()
+        self.mainLayout = QVBoxLayout()
 
         self.setup_ui()
 
@@ -28,11 +32,22 @@ class Help(QWidget):
         with open(HELP_DOC_FILE_PATH, "r") as f:
            doc = md.markdown(f.read(),  extensions=['fenced_code', 'codehilite', 'tables', 'attr_list'])
 
-        webview = HtmlView()
-        webview.setHtml(doc)
+        self.webview = HtmlView()
+        self.webview.setHtml(doc)
 
-        self.mainLayout.addWidget(webview)
+        self.uploadButton.clicked.connect(self.update_help_doc)
+        self.mainLayout.addWidget(self.uploadButton, alignment=Qt.AlignRight)
+        self.mainLayout.addWidget(self.webview)
         self.setLayout(self.mainLayout)
+
+    def update_help_doc(self):
+        custom_doc_path= QFileDialog.getOpenFileName(self, "Select the Help document XML File", filter="Markdown (*.md)")
+        os.remove(HELP_DOC_FILE_PATH)
+        shutil.copyfile(custom_doc_path[0], HELP_DOC_FILE_PATH)
+        self.uploadButton.clicked.disconnect(self.update_help_doc)
+        self.mainLayout.removeWidget(self.uploadButton)
+        self.mainLayout.removeWidget(self.webview)
+        self.setup_ui()
 
 
 class WebEnginePage(QWebEnginePage):
@@ -46,3 +61,5 @@ class HtmlView(QWebEngineView):
     def __init__(self, *args, **kwargs):
         QWebEngineView.__init__(self, *args, **kwargs)
         self.setPage(WebEnginePage(self))
+
+
