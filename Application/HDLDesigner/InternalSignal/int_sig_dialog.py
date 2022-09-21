2685e825d73afdd36bec5e30c399fdbe730811c6
JP-Byrne <j.byrne34@nuigalway.ie>
2022-09-21 16:21:22 +0100

    Changes were made 21/09/22

--- Application/HDLDesigner/InternalSignal/int_sig_dialog.py
+++ Application/HDLDesigner/InternalSignal/int_sig_dialog.py
@@ -13,6 +13,8 @@ class IntSignalDialog(QDialog):
     def __init__(self, add_or_edit, intSig_data=None):
         super().__init__()
 
+        self.input_layout = QGridLayout()
+        
         if add_or_edit == "add":
             self.setWindowTitle("New Internal Signal")
         elif add_or_edit == "edit":
@@ -26,26 +28,28 @@ class IntSignalDialog(QDialog):
 
         self.sig_types = ["std_logic", "std_logic_vector"]
 
-        self.input_layout = QGridLayout()
 
         self.mainLayout = QVBoxLayout()
 
         self.intSig_name_label = QLabel("Internal Signal Name*")
         self.intSig_name_label.setStyleSheet(WHITE_COLOR)
         self.intSig_name_input = QLineEdit()
-        self.intSig_name_input.setFixedWidth(120)
+        #self.intSig_name_input.setFixedWidth(120)
 
         self.sig_type_label = QLabel("Signal Type")
         self.sig_type_label.setStyleSheet(WHITE_COLOR)
         self.sig_type_combo = QComboBox()
-        self.sig_type_combo.setFixedWidth(110)
+        pal = self.sig_type_combo.palette()
+        pal.setColor(QPalette.Button, QColor(255, 255, 255))
+        self.sig_type_combo.setPalette(pal)
+        #self.sig_type_combo.setFixedWidth(110)
         self.sig_type_combo.addItems(self.sig_types)
 
         self.sig_size_label = QLabel("Size (eg. 32) * ")
         self.sig_size_label.setStyleSheet(WHITE_COLOR)
         self.sig_size_input = QLineEdit()
         self.sig_size_input.setText("1")
-        self.sig_size_input.setFixedWidth(100)
+        #self.sig_size_input.setFixedWidth(100)
         self.sig_size_input.setEnabled(False)
 
         self.onlyInt = QIntValidator()
@@ -53,9 +57,10 @@ class IntSignalDialog(QDialog):
 
         self.sig_desc_label = QLabel("Signal Description")
         self.sig_desc_label.setStyleSheet(WHITE_COLOR)
+        #self.sig_desc_label.setFixedWidth(120)
         self.sig_desc_input = QLineEdit()
 
-        self.sig_layout = QHBoxLayout()
+        #self.sig_layout = QHBoxLayout()
 
 
         self.cancel_btn = QPushButton("Cancel")
@@ -79,30 +84,30 @@ class IntSignalDialog(QDialog):
 
         self.setup_ui()
 
-        if add_or_edit == "edit":
+        if add_or_edit == "edit" and intSig_data != None:
             self.load_sig_data(intSig_data)
 
     def setup_ui(self):
 
         self.input_layout.addWidget(self.intSig_name_label, 0, 0, 1, 1)
-        self.input_layout.addWidget(self.intSig_name_input, 1, 0, 1, 2)
-        self.input_layout.addWidget(self.sig_type_label, 0, 3, 1, 1)
-        self.input_layout.addWidget(self.sig_type_combo, 1, 3, 1, 2)
-        self.input_layout.addWidget(self.sig_size_label, 0, 5, 1, 1)
-        self.input_layout.addWidget(self.sig_size_input, 1, 5, 1, 2)
-        self.input_layout.addWidget(self.sig_desc_label, 2, 0, 1, 1)
-        self.input_layout.addWidget(self.sig_desc_input, 3, 0, 1, 7)
-        self.input_layout.addItem(QSpacerItem(0, 10), 4, 0, 1, 5)
-        self.input_layout.addWidget(self.cancel_btn, 5, 4, 1, 1, alignment=Qt.AlignRight)
-        self.input_layout.addWidget(self.ok_btn, 5, 5, 1, 2, alignment=Qt.AlignRight)
+        self.input_layout.addWidget(self.intSig_name_input, 1, 0, 1, 3)
+        self.input_layout.addWidget(self.sig_type_label, 2, 0, 1, 1)
+        self.input_layout.addWidget(self.sig_type_combo, 3, 0, 1, 2)
+        self.input_layout.addWidget(self.sig_size_label, 2, 2, 1, 1)
+        self.input_layout.addWidget(self.sig_size_input, 3, 2, 1, 1)
+        self.input_layout.addWidget(self.sig_desc_label, 4, 0, 1, 1)
+        self.input_layout.addWidget(self.sig_desc_input, 5, 0, 1, 3)
+        self.input_layout.addItem(QSpacerItem(0, 20), 6, 0, 1, 3)
+        self.input_layout.addWidget(self.cancel_btn, 7, 1, 1, 1, alignment=Qt.AlignRight)
+        self.input_layout.addWidget(self.ok_btn, 7, 2, 1, 1, alignment=Qt.AlignRight)
 
-        self.intSig_name_input.textChanged.connect(self.enable_ok_btn);
         self.input_frame.setFrameShape(QFrame.StyledPanel)
         self.input_frame.setStyleSheet('.QFrame{background-color: rgb(97, 107, 129); border-radius: 5px;}')
         self.input_frame.setContentsMargins(10, 10, 10, 10)
-        self.input_frame.setFixedSize(400, 175)
+        self.input_frame.setFixedSize(400, 250)
         self.input_frame.setLayout(self.input_layout)
-
+        
+        self.intSig_name_input.textChanged.connect(self.enable_ok_btn);
         self.sig_type_combo.currentTextChanged.connect(self.enable_size_option)
 
         self.ok_btn.clicked.connect(self.get_data)
@@ -121,10 +126,13 @@ class IntSignalDialog(QDialog):
 
     def get_data(self):
         data = []
+        intSignalDescription = self.sig_desc_input.text()
+        if intSignalDescription == "":
+            intSignalDescription = "to be completed"
         data.append(self.intSig_name_input.text())
         data.append(self.sig_type_combo.currentText())
         data.append(self.sig_size_input.text())
-        data.append(self.sig_desc_input.text())
+        data.append(intSignalDescription)
         self.cancelled = False
         self.close()
         return data
