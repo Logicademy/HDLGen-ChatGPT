20d8f73da9522287d816cad4a369edd906fcde34
JP-Byrne <j.byrne34@nuigalway.ie>
2023-01-20 16:34:12 +0000

    Changed how arrays are created and accessed.

---
+++ Application/HDLDesigner/Package/mainPackage.vhd
@@ -0,0 +1,10 @@
+-- Package
+library ieee;
+use ieee.std_logic_1164.all;
+package arrayPackage is
+type array4x32 is array(4 downto 0) of std_logic_vector(32 downto 0);
+type array5x32 is array(5 downto 0) of std_logic_vector(32 downto 0);
+end arrayPackage;
+package body arrayPackage is
+end arrayPackage;
+
