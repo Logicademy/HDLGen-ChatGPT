acd8692162d65b033ac87fed6f4e41115afb62fe
JP-Byrne <j.byrne34@nuigalway.ie>
2023-02-23 22:54:41 +0000

    Fixed array problem with signed and unsigned

--- Application/HDLDesigner/Package/mainPackage.vhd
+++ Application/HDLDesigner/Package/mainPackage.vhd
@@ -1,6 +1,7 @@
 -- Package
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 package arrayPackage is
 type array3x12 is array(3 downto 0) of std_logic_vector(12 downto 0);
 type array3x12_signed is array(3 downto 0) of signed(12 downto 0);
